b3a93a19a0ab6ec80b8bfce038f7c85c6dd3f813
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2023-11-03 21:29:47 -0300

    exercicio 3

--- AFS/AF-3.4/ex-3.py
+++ AFS/AF-3.4/ex-3.py
@@ -1,28 +1,42 @@
 from multiprocessing import Process, Queue
 
 def origem(fila1):
-  for i in range(10):
-    msg = 'Teste ' + str(i)
-    # envia mensagem para a ponte pela fila 1
-    print('Mensagem enviada:', msg)
+    for i in range(10):
+        msg = 'Teste ' + str(i)
+        fila1.put(msg)  # envia mensagem para a ponte pela fila 1
+        print('Mensagem enviada:', msg)
 
 def ponte(fila1, fila2):
-  for i in range(10):
-    # recebe mensagem na fila 1
-    # reenvia mensagem na fila 2
-    print('Mensagem encaminhada:', msg)
+    for i in range(10):
+        msg = fila1.get()  # recebe mensagem na fila 1
+        fila2.put(msg)  # reenvia mensagem na fila 2
+        print('Mensagem encaminhada:', msg)
 
 def destino(fila2):
-  for i in range(10):
-    # recebe mensagem na fila 2
-    print('Mensagem recebida:', msg)
+    for i in range(10):
+        msg = fila2.get()  # recebe mensagem na fila 2
+        print('Mensagem recebida:', msg)
 
 if __name__ == '__main__':
-  fila1 = Queue()
-  fila2 = Queue()
-  o = Process(target=origem, args=(fila1,))
-  p = Process(target=ponte, args=(fila1,fila2))
-  d = Process(target=destino, args=(fila2,))
-  # executa os processos
-  # aguarda conclusão
-  # libera as filas
+    fila1 = Queue()
+    fila2 = Queue()
+    
+    o = Process(target=origem, args=(fila1,))
+    p = Process(target=ponte, args=(fila1, fila2))
+    d = Process(target=destino, args=(fila2,))
+    
+    # executa os processos
+    o.start()
+    p.start()
+    d.start()
+    
+    # aguarda conclusão
+    o.join()
+    p.join()
+    d.join()
+    
+    # libera as filas
+    fila1.close()
+    fila2.close()
+    fila1.join_thread()
+    fila2.join_thread()
